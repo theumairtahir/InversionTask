94c80ea57d61a698eb56f420ff19bc6063daff23
Umair Tahir <theumairtahir@gmail.com>
2024-05-02 10:00:26 +0500

    added person entity

---
+++ Inversion.FamilyTree.Domain/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Inversion.FamilyTree.Domain.Entities;
+
+public class Person
+{
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public required string SurName { get; set; }
+	public DateOnly BirthDate { get; set; }
+	public required string IdentityNumber { get; set; }
+	public Person? Father { get; set; }
+	public Person? Mother { get; set; }
+}
